e511f7dbddb2187acceca3b952b2bb32a0cdda2e
Fig Hawkins <fig.hawkins@pcc.edu>
2023-11-03 15:27:38 -0700

    updated prompts

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ document.getElementById('sceneButton').addEventListener('click', function() {
 async function fetchDnDScene() {
     try {
         const response = await fetch('/generate-scene', { method: 'POST' });
-        if (!response.ok) {
-            throw new Error('Network response was not ok: ' + response.statusText);
-        }
-        const data = await response.json();
-
-        if(data.scene) {
-            const display = document.getElementById('sceneDisplay');
-            display.textContent = data.scene;
-            display.style.display = 'block';
+        if (response.ok) { // Check if the HTTP response is successful
+            const data = await response.json();
+            if (data.scene) {
+                const display = document.getElementById('sceneDisplay');
+                display.textContent = data.scene;
+                display.style.display = 'block';
+            } else {
+                alert('Error fetching scene. Please try again.');
+            }
         } else {
-            throw new Error('Scene data is not available.');
+            throw new Error('Network response was not ok.');
         }
     } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error.message);
         alert('Error fetching scene. Please try again.');
+        console.error('There has been a problem with your fetch operation:', error);
     }
 }
--- server.js
+++ server.js
@@ -1,42 +1,32 @@
-require('dotenv').config(); // Make sure to install the dotenv package
+require('dotenv').config();
 const express = require('express');
-const fetch = require('node-fetch');
+
 const app = express();
 const port = 3000;
 
-// Use the .env file for your API key
-const API_KEY = process.env.OPENAI_API_KEY;
+const { OpenAI } = require('openai');
 
-// Middleware for parsing JSON bodies
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
 app.use(express.json());
-
-// Serve your static files
 app.use(express.static('.'));
 
 app.post('/generate-scene', async (req, res) => {
-    const prompt = "Make an awesome DnD scene idea in two sentences";
-    const response = await fetch('https://api.openai.com/v1/completions', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${API_KEY}`
-        },
-        body: JSON.stringify({
-            model: "text-davinci-003",
-            prompt: prompt,
-            max_tokens: 60
-        })
-    });
+    try {
+        const response = await openai.chat.completions.create({
+            model: "gpt-4",
+            messages: [{ role: "system", content: "you are a wise old storyteller. in two sentences, write a specific and creative, fun, scene idea for a role playing game"}],
+        });
 
-    if (response.ok) { // Check if the HTTP response is successful
-        const data = await response.json();
-        if (data.choices && data.choices.length > 0 && data.choices[0].text) {
-            res.json({ scene: data.choices[0].text.trim() });
+        if (response.choices && response.choices.length > 0) {
+            res.json({ scene: response.choices[0].message.content.trim() });
         } else {
             res.status(500).json({ error: 'Failed to generate scene' });
         }
-    } else {
-        // Handle the case where the OpenAI API call wasn't successful
+    } catch (error) {
+        console.error(error);
         res.status(500).json({ error: 'Failed to communicate with OpenAI API' });
     }
 });
