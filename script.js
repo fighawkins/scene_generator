7ceee24b94ffa041f884f163acb228d95149a617
Fig Hawkins <fig.hawkins@pcc.edu>
2023-11-06 14:12:48 -0800

    initial styling, type effect in place

--- script.js
+++ script.js
@@ -5,20 +5,31 @@ document.getElementById('sceneButton').addEventListener('click', function() {
 async function fetchDnDScene() {
     try {
         const response = await fetch('/generate-scene', { method: 'POST' });
-        if (response.ok) { // Check if the HTTP response is successful
-            const data = await response.json();
-            if (data.scene) {
-                const display = document.getElementById('sceneDisplay');
-                display.textContent = data.scene;
-                display.style.display = 'block';
-            } else {
-                alert('Error fetching scene. Please try again.');
-            }
+        if (!response.ok) {
+            throw new Error('Network response was not ok: ' + response.statusText);
+        }
+        const data = await response.json();
+
+        if (data.scene) {
+            const display = document.getElementById('sceneDisplay');
+            display.style.display = 'block';
+
+            // Initialize Typed.js on the #sceneDisplay element with the fetched scene text
+            new Typed('#typedText', {
+                strings: [data.scene],
+                typeSpeed: 50,
+                backSpeed: 25,
+                smartBackspace: false,
+                loop: false,
+                showCursor: true,
+                cursorChar: '|',
+                contentType: 'html'
+            });
         } else {
-            throw new Error('Network response was not ok.');
+            throw new Error('Scene data is not available.');
         }
     } catch (error) {
-        alert('Error fetching scene. Please try again.');
         console.error('There has been a problem with your fetch operation:', error);
+        alert('Error fetching scene. Please try again.');
     }
 }
